40eb3556c004d89810c15605d1e7d098a1a50e82
omersagy <omersagyemails@gmail.com>
2018-03-10 22:21:11 -0500

    player

--- player.py
+++ player.py
@@ -18,7 +18,7 @@ def play_episode(episode: t.Episode, state: s.GameState) -> None:
 
 def play_action(action: t.Action, state: s.GameState) -> None:
     if isinstance(action, t.Narration):
-        play_narration(action, state)
+        play_narration(action)
     elif isinstance(action, t.Dialogue):
         play_dialogue(action, state)
     elif isinstance(action, t.Alias):
@@ -29,8 +29,23 @@ def play_action(action: t.Action, state: s.GameState) -> None:
         play_fork(action, state)
 
 
-def play_narration(narration: t.Narration, state: s.GameState) -> None:
+def play_narration(narration: t.Narration) -> None:
+    p.print_line(narration.text)
 
 
+def play_dialogue(dialogue: t.Dialogue,
+                  state: s.GameState) -> None:
+    speaker_display_name = str(look_up_character(
+        dialogue.speaker_id, state))
 
+    p.print_line('%s: %s' % (speaker_display_name,
+                             dialogue.text))
+
+
+def look_up_character(speaker_id: str,
+                      state: s.GameState) -> s.Character:
+    for c in state.characters:
+        if (c.real_name == speaker_id
+                or c.code_name == speaker_id):
+            return c
 
